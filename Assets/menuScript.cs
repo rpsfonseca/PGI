37defd979d5fe93815bed5f2058f25ad971f827c
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-10-29 00:52:55 +0000

    functional simple menu

---
+++ Assets/menuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+
+
+	void Start () {
+	
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPressed(){
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPressed() {
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+	public void StartLevel() {
+		//System.Console.WriteLine ("Started game");
+
+	}
+}
