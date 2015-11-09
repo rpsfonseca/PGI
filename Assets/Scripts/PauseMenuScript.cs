fc753bad35428f85823dca6d62e6f6ffa1678c04
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-11-09 13:47:55 +0000

    Add functioning pause menu

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuScript : MonoBehaviour {
+
+	bool isPaused = false;
+	public GameObject pausePanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckIfEscPressed ();
+	}
+
+	void CheckIfEscPressed(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if(isPaused == false){
+				PauseGame();
+			}
+			else{
+				ResumePressed();
+			}
+		}
+	}
+
+	void PauseGame(){
+		pausePanel.SetActive(true);
+		isPaused = true;
+		Time.timeScale = 0;
+	}
+
+	public void ResumePressed(){
+		pausePanel.SetActive(false);
+		isPaused = false;
+		Time.timeScale = 1;
+
+	}
+	
+
+	public void OptionPressed() {
+		//WIP
+	}
+
+	public void MainMenuPressed() {
+		//Need confirmation panel here
+		Application.LoadLevel (0);
+	}
+
+	public void ExitPressed() {
+
+		//Need confirmation panel here
+		Application.Quit ();
+	}
+}
