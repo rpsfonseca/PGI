6d0ede05f7afe3db61f53fdb7f35247a651bd2ba
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-11-21 03:08:49 +0000

    Initial prototype for third level

--- Assets/Scripts/Base/BaseCamera.cs
+++ Assets/Scripts/Base/BaseCamera.cs
@@ -16,6 +16,6 @@ public class BaseCamera : MonoBehaviour {
 	}
 
 	void CameraXFollow() {
-		GetComponent<Camera>().transform.position = new Vector3 (character.transform.position.x, GetComponent<Camera>().transform.position.y, -11);
+		GetComponent<Camera>().transform.position = new Vector3 (character.transform.position.x, character.transform.position.y, -11);
 	}
 }
