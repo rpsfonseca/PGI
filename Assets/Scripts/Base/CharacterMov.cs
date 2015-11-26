d556155756f1ccc634cafb9eb5feb9fa351e082f
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-11-26 22:12:19 +0000

    Solve bug from level2

--- Assets/Scripts/Base/CharacterMov.cs
+++ Assets/Scripts/Base/CharacterMov.cs
@@ -29,6 +29,7 @@ public class CharacterMov : MonoBehaviour {
 		 
 		float move = Input.GetAxis ("Horizontal");
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		Debug.Log ("Speed : " + move * maxSpeed);
 
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
@@ -40,7 +41,7 @@ public class CharacterMov : MonoBehaviour {
 	}
 
 	void Update() {
-		Debug.Log (grounded);
+
 		verticalDeath ();
 		Jump ();
 
