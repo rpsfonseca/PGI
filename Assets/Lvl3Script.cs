f8f9f83d0050dd3cd8942984f7fa6c64dcb323ff
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-11-27 18:17:46 +0000

    Add balloon animator

---
+++ Assets/Lvl3Script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lvl3Script : MonoBehaviour {
+
+	public GameObject balloon;
+
+	public Animator anim;
+
+
+	// Use this for initialization
+
+	float originalX;
+	float originalY;
+	float originalZ;
+
+	void Start () {
+
+		originalX = balloon.transform.localScale.x;
+		originalY = balloon.transform.localScale.y;
+		originalZ = balloon.transform.localScale.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		increaseBalloon ();
+	}
+
+	void increaseBalloon(){
+		if (Input.GetKeyDown (KeyCode.R)) {
+			anim.SetTrigger("Rpressed");
+			/*if(balloon.transform.localScale.x > 4)
+				balloon.transform.localScale = new Vector3(originalX,originalY,originalZ);
+			else
+				balloon.transform.localScale += new Vector3(1f,1f,0f); */
+		}
+
+	}
+}
