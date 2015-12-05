5f8910cec9bc4887a489c5d42e93f60ce94f77c6
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-05 15:53:43 +0000

    Script noew generate random color balloon yey, coloooors

---
+++ Assets/Scripts/EndLevel1/HeliumWorker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeliumWorker : MonoBehaviour {
+
+	public GameObject balloon;
+	public GameObject character;
+	public Sprite[] balloonColor = new Sprite[6];
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		growBalloon ();
+	}
+
+	void growBalloon(){
+		if (Input.GetKeyDown (KeyCode.R) && Mathf.Abs (transform.position.x - character.transform.position.x) < 3 && balloon.transform.localScale.x == 0) {
+			balloon.GetComponent<SpriteRenderer>().sprite = balloonColor[Random.Range(0,5)];
+			balloon.GetComponent<Animator>().SetTrigger("BalloonGrow");
+
+		}
+		
+	}
+
+}
