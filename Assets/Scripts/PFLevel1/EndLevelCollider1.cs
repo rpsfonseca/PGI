d1127de3608f91dde8362578da88397f6ce2ca62
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-05 15:14:27 +0000

    Add balloon platform and assets

--- Assets/Scripts/PFLevel1/EndLevelCollider1.cs
+++ Assets/Scripts/PFLevel1/EndLevelCollider1.cs
@@ -5,6 +5,7 @@ public class EndLevelCollider1 : MonoBehaviour {
 		
 	public GameObject script;
 	public int levelToLoad = 0;
+	bool isCompleted = false;
 	// Use this for initialization
 	void Start () {
 	}
@@ -16,6 +17,7 @@ public class EndLevelCollider1 : MonoBehaviour {
 		
 	void OnCollisionEnter2D(){
 		if(script.GetComponent<HintMessage1> ().getValid()){
+
 			Application.LoadLevel(levelToLoad);
 		}
 	}
