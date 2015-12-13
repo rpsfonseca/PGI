4ab9e845de6c7ec06c982e95a08de9f5f87174e8
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-13 19:33:18 +0000

    initial cross session saves

--- Assets/Scripts/MainMenu/LevelMenuScript.cs
+++ Assets/Scripts/MainMenu/LevelMenuScript.cs
@@ -4,28 +4,38 @@ using UnityEngine.UI;
 
 public class LevelMenuScript : MonoBehaviour {
 
-	bool[] completedTable;
+	static bool[] completedTable = new bool[3];
+	static int level1 = 0;
+	static int level2 = 0;
 	public Sprite[] imageArray = new Sprite[3];
 	public Sprite locked;
 
 
+
 	void Start () {
-		completedTable = new bool[3];
 		completedTable [0] = true;
+		//PlayerPrefs.SetInt ("lvl2", level2);
+		//level2 = PlayerPrefs.GetInt ("lvl2");
+
+		DontDestroyOnLoad(this.gameObject);
 
 
-		if (completedTable [1] == true) {
+		if (completedTable [1] == true || PlayerPrefs.HasKey("lvl2")) {
 			transform.FindChild("Level2Image_btn").GetComponent<Image>().sprite = imageArray [1];
 		} else {
 			transform.FindChild("Level2Image_btn").GetComponent<Image>().sprite = locked;
 		}
 
-		if (completedTable [2] == true) {
+		if (completedTable [2] == true || PlayerPrefs.HasKey("lvl3")) {
 			transform.FindChild("Level3Image_btn").GetComponent<Image>().sprite = imageArray [2];
 		} else {
 			transform.FindChild("Level3Image_btn").GetComponent<Image>().sprite = locked;
 		}
 
+	}
+
+	public void setCompletedTableTrue(int level){
+		completedTable [level - 1] = true;
 	}
 	
 
--- Assets/Scripts/PFLevel1/EndLevelCollider1.cs
+++ Assets/Scripts/PFLevel1/EndLevelCollider1.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class EndLevelCollider1 : MonoBehaviour {
 		
 	public GameObject script;
+	//LevelMenuScript lvlMenuScripts;
 	public int levelToLoad = 0;
 	bool isCompleted = false;
 	// Use this for initialization
 	void Start () {
+
 	}
 		
 	// Update is called once per frame
@@ -17,7 +19,7 @@ public class EndLevelCollider1 : MonoBehaviour {
 		
 	void OnCollisionEnter2D(){
 		if(script.GetComponent<HintMessage1> ().getValid()){
-
+			PlayerPrefs.SetInt("lvl2",1);
 			Application.LoadLevel(levelToLoad);
 		}
 	}
