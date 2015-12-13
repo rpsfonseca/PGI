6af9e483fe1803d251ee4753d76cfd5b5373cbd9
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-13 20:03:17 +0000

    Add third level limits, end, and menu control

---
+++ Assets/Scripts/PFLevel3/DeathByFall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathByFall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.name.Equals ("Character")) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
