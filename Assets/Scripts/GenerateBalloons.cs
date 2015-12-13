71ceaeed58d5ed5d76264f34864e0cd84104cd25
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-13 15:23:05 +0000

    Fourth level balloon generator

---
+++ Assets/Scripts/GenerateBalloons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateBalloons : MonoBehaviour {
+
+	public GameObject balloon;
+
+	// Use this for initialization
+	void Start () {
+		StartGeneratingEggs ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void StartGeneratingEggs(){
+		InvokeRepeating ("GenerateBalloon", 1f, 2f);
+	}
+	
+	
+	void GenerateBalloon(){
+		Instantiate (balloon);
+	}
+
+
+
+}
---
+++ Assets/Scripts/WaterBalloonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBalloonBehaviour : MonoBehaviour {
+
+	public GameObject launcher;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = launcher.transform.position;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (10, 0);
+	}
+
+
+}
