f974f5b5afe9d61c3c8e6337a31044cbf155edf2
Ricardo Fonseca <Ricardo Fonseca>
2015-12-27 13:28:56 +0000

    Physics info display working at the end of each level

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour {
+    int levelToLoad;
+    public Text infoText;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        levelToLoad = PlayerPrefs.GetInt("lastlevel") + 1;
+        ChangeInfo(levelToLoad);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,9 +19,24 @@ public class SceneController : MonoBehaviour {
 	}
 
 	public void ButtonDoAction(){
-		int levelToLoad = PlayerPrefs.GetInt ("lastlevel") + 1;
 		if (levelToLoad == 7)
 			Application.LoadLevel (0);
 		Application.LoadLevel (levelToLoad);
 	}
+
+    public void ChangeInfo(int lvl) {
+        /*if (lvl == 1) {
+            infoText.text = "Use A to move to the left and D \nto move to the right!";
+        }
+        else if (lvl == 2) {
+            infoText.text = "Great! Now go to the right by pressing D!";
+        }
+        else if (lvl == 3) {
+            infoText.text = "Great! Now go to the left by pressing A!";
+        }
+        else if (lvl == 4) {
+            infoText.text = "Good! Now you got it! Pass the sign with the \narrow  to go to the next level!";
+        }*/
+        infoText.text = "*PLACEHOLDER, WAITING FOR TEXTS*";
+    }
 }
