ffdd35549458673bc6da688a0a39e86c0044470a
Pedro Vide Simões <pedrovide1994@hotmail.com>
2015-12-28 17:31:37 +0000

    Failure

--- Assets/Scripts/Base/BaseCamera.cs
+++ Assets/Scripts/Base/BaseCamera.cs
@@ -8,15 +8,23 @@ public class BaseCamera : MonoBehaviour {
 	bool mapOn = false;
 	float originalSize;
 
-	//public Transform firstWall;
-	//public Transform LastWall;
+	public Transform firstWall;
+	public Transform LastWall;
+
+	bool stopCam = false;
 
 	//float max;
 	//float min;
 
 	// Use this for initialization
 	void Start () {
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Character"),
+		                               LayerMask.NameToLayer("Camera"),
+		                               true
+		                               );
 		originalSize = GetComponent<Camera> ().orthographicSize;
+		//GetComponent<BoxCollider2D> ().size = new Vector2 (GetComponent<Camera> ().WorldToViewportPoint(transform.position).x, GetComponent<Camera> (transform.position).WorldToViewportPoint().y);
+		GetComponent<BoxCollider2D> ().size = new Vector2 (GetComponent<Camera> ().orthographicSize * 3, GetComponent<Camera> ().orthographicSize * 2);
 
 	//	min = firstWall.position.x + 7.5f;
 	//	max = LastWall.position.x - 7.5f;
@@ -39,18 +47,39 @@ public class BaseCamera : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
+
 		CameraXFollow ();
 		Mpressed ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log (col.name);
+		if (col.gameObject.name.Equals (firstWall.name)) {
+			Debug.Log ("entrou no cam collider");
+			// || col.gameObject.name.Equals(LastWall.name)) {
+			Debug.Log ("pos a true");
+			stopCam = true;
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D col){
+		if (col.gameObject.name.Equals (firstWall.name)) {
+			Debug.Log ("ola");
+			stopCam = false;
+		}
 	}
 
 	void CameraXFollow() {
 		if (mapOn == false) {
 		//	if(character.transform.position.x > min && character.transform.position.x < max){
-			
+			if(stopCam == false){
 			GetComponent<Camera> ().transform.position = new Vector3 (character.transform.position.x, character.transform.position.y + yOffSet, -11);
+			}
 	//		}
-	//		else{
-	//			GetComponent<Camera> ().transform.position = new Vector3 (GetComponent<Camera>().transform.position.x, character.transform.position.y + yOffSet, -11);
+			else{
+				Debug.Log ("O stopcam esta  true");
+				GetComponent<Camera> ().transform.position = new Vector3 (GetComponent<Camera>().transform.position.x, character.transform.position.y + yOffSet, -11);
+			}
 	//		}
 		}
 	}
