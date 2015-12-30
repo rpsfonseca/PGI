9fae03b7035e1c5d2a1b55f3bce2a5f52540b6ac
Ricardo Fonseca <Ricardo Fonseca>
2015-12-30 13:32:31 +0000

    Fix last scene transition

--- Assets/Scripts/EndLevel1/EndLevelColliderLastLvl.cs
+++ Assets/Scripts/EndLevel1/EndLevelColliderLastLvl.cs
@@ -20,7 +20,7 @@ public class EndLevelColliderLastLvl : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name.Equals("Character")){
 			PlayerPrefs.SetInt("lvl7",1);
-			PlayerPrefs.SetInt ("lastlevel",6);
+			PlayerPrefs.SetInt ("lastlevel",8);
 			Application.LoadLevel(8);
 		}
 	}
